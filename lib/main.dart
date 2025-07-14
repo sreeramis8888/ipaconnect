ce2cbc75e55000d7a0c3ef9b48a756e0073d8c93
sreeramis8888 <128227002+sreeramis8888@users.noreply.github.com>
2025-07-14 09:27:44 -0700

    ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -22,6 +23,7 @@ Future<void> main() async {
   );
   await loadSecureData();
   await dotenv.load(fileName: ".env");
+    FirebaseAuth.instance.setSettings(appVerificationDisabledForTesting: false);
   Stripe.publishableKey = dotenv.env['STRIPE_KEY'] ?? '';
   runApp(ProviderScope(child: MyApp()));
 }
