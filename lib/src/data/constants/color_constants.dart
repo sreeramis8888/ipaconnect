23a7f630cbac717f2415a10850da5921a42ca834
SHIDUL PP <shidulpp@gmail.com>
2025-09-10 17:30:31 +0530

    changed colour constants for new theme

--- lib/src/data/constants/color_constants.dart
+++ lib/src/data/constants/color_constants.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
-const Color kPrimaryColor = Color(0xFF0D74BC);
-const Color kBackgroundColor = Color(0xFF0D0F22);
-const Color kCardBackgroundColor = Color(0xFF021730);
+// const Color kPrimaryColor = Color(0xFF0D74BC);
+const kPrimaryColor = Color(0xFF00AEEF); // cyan accent
+const kSecondaryColor = Color(0xFF145DA0); // optional darker accent
+// const Color kBackgroundColor = Color(0xFF0D0F22);
+const kBackgroundColor = Color(0xFF0A1931); // deep navy
+// const Color kCardBackgroundColor = Color(0xFF021730);
+const kCardBackgroundColor = Color(0xFF102542); // slightly lighter navy for cards
+
 const Color kPrimaryLightColor = Color(0xFFEEF1FF);
-const Color kStrokeColor = Color(0xFF0D2A4D);
+// const Color kStrokeColor = Color(0xFF0D2A4D);
+const kStrokeColor = Color(0xFF1E3C72); // thin borders
+
 
 const Color kTertiary = Color(0xFFE8EAED);
 const Color kInputFieldcolor = Color(0xFF1C1E4D);
@@ -14,7 +21,9 @@ const Color kGrey = Color.fromARGB(255, 200, 200, 200);
 
 const Color kTextColor = Color(0xFFF7F7F7);
 
-const Color kSecondaryTextColor = Color(0xFFAEB9E1);
+// const Color kSecondaryTextColor = Color(0xFFAEB9E1);
+const kSecondaryTextColor = Color(0xFFB3E5FC); // light cyan for subtitles
+
 
 const Color kGreyLight = Color(0xFFCCCCCC);
 const Color kGreyDark = Color.fromARGB(255, 118, 121, 124);
@@ -23,6 +32,6 @@ const Color kRed = Color(0xFFEB5757);
 const Color kRedDark = Color(0xFFC9300E);
 const Color kBlack = Color.fromARGB(255, 5, 5, 5);
 const Color kBlack54 = Color(0xFF8A000000);
-const Color kGreen = Color.fromARGB(255, 76, 175, 80);
+const Color kGreen = Color.fromARGB(255, 76, 175, 80);  
 const Color kBlue = Color(0xFF2B74E1);
 const Color kLightGreen = Color.fromARGB(255, 192, 252, 194);
