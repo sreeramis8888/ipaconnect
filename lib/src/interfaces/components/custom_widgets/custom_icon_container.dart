cee9fb0b8a05ca6287cbc15903669c79194a2b5a
SHIDUL PP <shidulpp@gmail.com>
2025-09-11 12:18:54 +0530

    changed quick action button colours

--- lib/src/interfaces/components/custom_widgets/custom_icon_container.dart
+++ lib/src/interfaces/components/custom_widgets/custom_icon_container.dart
@@ -23,19 +23,30 @@ class CustomIconContainer extends StatelessWidget {
         height: size + 10,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16.0),
-          gradient: const RadialGradient(
-            center: Alignment.topLeft,
-            radius: 1.2,
+          gradient: const LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
             colors: [
-              Color(0x802EA7FF),
-              Color(0x331C1B33),
+              // Color(0x802EA7FF),
+              Color(0xFF10243F),
+              // Color(0x331C1B33),
+              Color(0xFF1C3559), 
             ],
             stops: [0.0, .7],
           ),
           border: Border.all(
-            color: Color(0x1A17B9FF),
-            width: 1.06,
+            color: 
+            // Color(0x1A17B9FF),
+            Color(0x33FFFFFF),
+            width: 1,
           ),
+          boxShadow: const [
+            BoxShadow(
+              color: Color(0x6617B9FF), 
+              blurRadius: 8,
+              offset: Offset(2, 2),
+            ),
+          ],
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
